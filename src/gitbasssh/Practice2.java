f657ec163512f347f2e2a64edb40aaef22c72538
murgeshnb <mnbechetti@gmail.com>
2022-01-31 22:36:24 +0530

    practice branch comment

---
+++ src/gitbasssh/Practice2.java
@@ -0,0 +1,12 @@
+package gitbasssh;
+
+public class Practice2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("comment form branch");
+
+	}
+
+}
