dc5dbafd55b5fa8fa33290255225d4ee3869b384
murgeshnb <mnbechetti@gmail.com>
2022-02-01 07:54:28 +0530

    Second comment for git bash practice from master

--- src/gitbasssh/Practice1.java
+++ src/gitbasssh/Practice1.java
@@ -3,12 +3,8 @@ package gitbasssh;
 public class Practice1 {
 
 	public static void main(String[] args) {
-
-
 		System.out.println("First practice");
-		
 		System.out.println("kanta jote rekha");
-
 	}
 
 }
